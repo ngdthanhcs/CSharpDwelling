2f253c1d51a14e018424453ce2308bf2b3fa560e
ThanhND2 <thanhnd2@ieccorp.vn>
2024-01-13 19:41:45 +0700

    12013024

---
+++ Workout/1347MinimumNumberOfStepsToMakeTwoStringsAnagram.cs
@@ -0,0 +1,45 @@
+﻿namespace Workout;
+
+public class MinimumNumberOfStepsToMakeTwoStringsAnagram {
+    public int MinSteps(string s, string t)
+    {
+        /*var dict = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            dict.TryAdd(c, 0);
+            dict[c]++;
+        }
+
+        foreach (var c in t)
+        {
+            dict.TryAdd(c, 0);
+            dict[c]--;
+        }
+
+        var count = 0;
+        foreach (var pair in dict)
+        {
+            count += Math.Abs(pair.Value);
+        }
+
+        return count;*/
+        
+        int[] charFrequency = new int[26];
+      
+        foreach (var c in s)
+        {
+            charFrequency[c - 'a']++;
+        }
+      
+        int minSteps = 0;
+      
+        foreach (var c in t)
+        {
+            if (--charFrequency[c - 'a'] < 0) {
+                minSteps++;
+            }
+        }
+        return minSteps;
+    }
+}
